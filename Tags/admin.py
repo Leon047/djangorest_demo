33a1f049170777ddbaeb6ac36132e091eb725627
Mr.Leon <levan.melkonian@gmail.com>
2020-11-08 14:41:47 +0200

    models

--- Tags/admin.py
+++ Tags/admin.py
@@ -5,9 +5,10 @@ from .models import Tags, Users
 
 @admin.register(Tags)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('tags',)
-    list_filter = ('id',)
+    list_display = ('id','languages', 'tags')
+    list_filter = ('languages', 'tags')
 
 @admin.register(Users)
 class Users(admin.ModelAdmin):
-    list_display = ('id', 'users')
+    list_display = ('id', 'user_name', 'tags')
+    list_filter = ('tags',)
--- Tags/models.py
+++ Tags/models.py
@@ -4,7 +4,15 @@ from taggit.managers import TaggableManager
 
 
 class Tags(models.Model):
+    languages = models.CharField(max_length=50, verbose_name='Knowledge & skills')
     tags = TaggableManager()
 
+    def __str__(self):
+        return self.languages
+
 class Users(models.Model):
-    users = models.CharField(max_length=200)
+    user_name = models.CharField(max_length=50, verbose_name='User name')
+    tags = models.ForeignKey(Tags, on_delete=models.CASCADE, verbose_name='Knowledge & skills')
+
+    def __str__(self):
+        return self.user_name
